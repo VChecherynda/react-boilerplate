89d3e550477e9ec7f39c1b976b7fd6b7c76ad290
Vadym <vadym.checherynda@litslink.com>
2019-04-09 15:27:17 +0300

    Change axios

--- src/sagas/api.js
+++ src/sagas/api.js
@@ -1,46 +1,44 @@
 import axios from 'axios';
 
-const headers = (token) => {
-  if (token) {
-    return {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    }
-  }
+const UNAUTHORIZED_HTTP_CODE = 401;
 
-  return {
-    headers: {
-      'Content-type': "application/x-www-form-urlencoded"
+const extractError = data => {
+  let msg;
+  if (Array.isArray(data))
+    msg = data.join('; ');
+  else if (typeof data === 'string')
+    msg = data;
+  else if (typeof data === 'object')
+    msg = data.error;
+  else
+    msg = 'Error occurred! Please, try again.';
+  throw new Error(msg);
+};
+
+const defaultOptions = {
+  method: 'GET',
+  validateStatus: status => {
+    if (status == UNAUTHORIZED_HTTP_CODE) {
+      throw new Error('User is not authorized!');
     }
+    return status;
   }
 };
 
-const data = (config) => {
-  if (config) {
-    return {
-      data: {
-        ...config
-      }
-    }
-  }
+export const apiRequest = async options => {
+  const { url } = options;
+  return await axios(
+    {
+      ...defaultOptions,
+      ...options,
+      url: `/${REACT_APP_URL}${url}`
+    })
+    .catch(error => {
+      const { response } = error;
+      if (!response) return error;
+      const { status, data } = response;
+      extractError(status >= 500 ? undefined : data);
+    });
+};
 
-  return null;
-}
-
-const request = (method, url, config = null, token = null) => {
-  axios({
-    method,
-    url: `${process.env.REACT_APP_URL}/${url}`,
-    ...data(config),
-    ...headers(token)
-  });
-}
-
-export const get = () => request('get', 'test');
-
-export const post = (data) => request('post', 'test', ...data);
-
-export const put = (data) => request('put', 'test', ...data, token);
-
-export const delete = (data) => request('delete', 'test', ...data, token);
+export const apiMutation = options => apiRequest({method: 'POST', ...options});
