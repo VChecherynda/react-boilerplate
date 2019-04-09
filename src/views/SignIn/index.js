272d9d47f8b963fd18469a9ed938e4a0e641687f
Vadym <vadym.checherynda@litslink.com>
2019-04-09 17:22:30 +0300

    Add default styles and 3 env variables

--- src/views/SignIn/index.js
+++ src/views/SignIn/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import classes from './styles.module.scss';
+
 const signIn = props => {
   return (
     <div>
       <h1>Sign In</h1>
-      <button onClick={props.onSubmit}>Submit</button>
+      <button onClick={props.onSubmit} className={classes.btnBg}>Submit</button>
     </div>
   );
 }
